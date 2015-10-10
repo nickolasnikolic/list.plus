89c2bc1b4df634a9d9b3032f558b8953a0715a31
Nickolas Nikolic <subclass@gmail.com>
2015-10-10 13:33:13 -0500

    testing new share approach

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -199,7 +199,8 @@ blindApp.controller('SharedController', ['$scope', '$state', '$stateParams', fun
           url: 'http://www.list.plus/app/#/shared/' + $scope.list.list_owner + '/' + $scope.list.id,
           title: $scope.list.title,
           ui:{
-            flyout: 'bottom right'
+            flyout: 'bottom right',
+            button_text: ''
           },
           networks: {
             facebook: {
