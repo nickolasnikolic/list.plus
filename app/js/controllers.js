a7ce3dcc568894a2aeebf255bcfa6c58ca001490
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-10-13 05:35:05 -0500

    major feature autocomplete

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -136,7 +136,8 @@ blindApp.controller('AdminController', ['$scope', '$state', 'globals', function(
     else{
       $.get( '../api/amazon/lookup/' + isbn )
           .success(function(data){
-            $scope.book = data.Items[0].Item.ItemAttributes;
+            data = JSON.parse(data);
+            $scope.book = data.Items.Item.ItemAttributes;
             console.log($scope.book);
           })
           .error(function(error){ console.log(error); });
